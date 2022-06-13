d0d955f7b3bfc49406dc5550034e38b07fef9c64
Adjie Indrawan <adjieindrawan@gmail.com>
2022-06-13 10:11:26 +0700

    setting up and route

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavbarApp from "../components/NavbarApp";
+
+const Home = () => {
+  return (
+    <div>
+      <NavbarApp />
+    </div>
+  );
+};
+
+export default Home;
