62e6a4971b9078affad41bac8b8927a7cf6bfc97
Adjie Indrawan <adjieindrawan@gmail.com>
2022-07-17 23:57:37 +0700

    responsive dashboard

--- src/pages/admin/user/index.js
+++ src/pages/admin/user/index.js
@@ -1,13 +1,12 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Layout, Space, Table, Tag } from "antd";
-import { Button } from 'react-bootstrap';
+import { Button } from "react-bootstrap";
 
 import SidebarAdmin from "../layouts/Sider";
 
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
-
-import {authApi} from '../../../api/api.auth';
+import { authApi } from "../../../api/api.auth";
 import moment from "moment";
 
 const { Header, Content, Footer } = Layout;
@@ -16,7 +15,7 @@ function AdminUser() {
   const [userList, setuserList] = useState([]);
   const [filterUser, setfilterUser] = useState({
     full_name: null,
-    email: null
+    email: null,
   });
 
   useEffect(() => {
@@ -25,34 +24,34 @@ function AdminUser() {
 
   const getUserList = async () => {
     const result = await authApi.userList(filterUser);
-    if(result.status === 'SUCCESS' && result.message === 'SUCCESS'){
+    if (result.status === "SUCCESS" && result.message === "SUCCESS") {
       setuserList(result.data);
     } else {
       setuserList([]);
-    }    
-  }
+    }
+  };
 
   const handleUserStatus = async (data, action) => {
-      const result = await authApi.updateUserStatus(action, data.id);
-      if(result.status === 'SUCCESS' && result.message === 'SUCCESS'){
-        await getUserList();
-        toast.success("Sukses." , {
-          position: "top-center",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          progress: undefined,
-          });
-      } else {
-        toast.error("Gagal." , {
-          position: "top-center",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          progress: undefined,
-          });
-      }
-  }
+    const result = await authApi.updateUserStatus(action, data.id);
+    if (result.status === "SUCCESS" && result.message === "SUCCESS") {
+      await getUserList();
+      toast.success("Sukses.", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        progress: undefined,
+      });
+    } else {
+      toast.error("Gagal.", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        progress: undefined,
+      });
+    }
+  };
 
   return (
     <Layout
@@ -72,7 +71,7 @@ function AdminUser() {
           style={{
             margin: "0 16px",
           }}
-        >          
+        >
           <div
             className="site-layout-background"
             style={{
@@ -83,67 +82,74 @@ function AdminUser() {
             <div className="mb-5 d-flex">
               <h4 className="w-100">Daftar Pengguna</h4>
             </div>
-            <Table             
-            columns={
-              [
-                  {
-                    title: "id",
-                    dataIndex: "id",
-                    key: "id",
-                    render: (text) => <p>{text}</p>,
-                  },
-                  {
-                    title: "Status",
-                    dataIndex: "user_status",
-                    key: "user_status",
-                    defaultSortOrder: 'ascend',
-                    sorter: (a, b) => a.user_status.length - b.user_status.length,
-                    render: (text) => <p>{text}</p>,
-                  },
-                  {
-                    title: "Nama",
-                    dataIndex: "full_name",
-                    key: "full_name",
-                    sorter: (a, b) => a.full_name.length - b.full_name.length,
-                    render: (text) => <p>{text}</p>,
-                  },
-                  {
-                    title: "Email",
-                    dataIndex: "email",
-                    key: "email",
-                    sorter: (a, b) => a.email.length - b.email.length,
-                    render: (text) => <p>{text}</p>,
-                  },
-                  {
-                    title: "Tgl Pendaftaran",
-                    dataIndex: "created_at",
-                    key: "created_at",
-                    sorter: (a, b) => a.created_at.length - b.created_at.length,
-                    render: (text) => <p>{moment(text).format('DD/MM/YYYY')}</p>,
-                  },
-                  {
-                    title: "Action",
-                    key: "action",
-                    render: (_, record) => (
-                      <Space size="middle">
-                          <Button
-                            onClick={()=> {
-                              handleUserStatus(record, record.user_status === 'Pembaca' ? 'SET_TO_AUTHOR' : 'SET_TO_READER');
-                            }}
-                            variant={record.user_status === 'Pembaca' ? "success" : 'primary'}
-                          >
-                            {
-                              record.user_status === 'Pembaca' ?
-                              'Jadikan Penulis'
-                              :
-                              'Jadikan Pembaca'
-                            }
-                            </Button>
-                      </Space>
-                    ),
-                  },                
-              ]
-            } dataSource={userList} />
+            <Table
+              scroll={{ x: 980 }}
+              columns={[
+                {
+                  title: "id",
+                  dataIndex: "id",
+                  key: "id",
+                  render: (text) => <p>{text}</p>,
+                },
+                {
+                  title: "Status",
+                  dataIndex: "user_status",
+                  key: "user_status",
+                  defaultSortOrder: "ascend",
+                  sorter: (a, b) => a.user_status.length - b.user_status.length,
+                  render: (text) => <p>{text}</p>,
+                },
+                {
+                  title: "Nama",
+                  dataIndex: "full_name",
+                  key: "full_name",
+                  sorter: (a, b) => a.full_name.length - b.full_name.length,
+                  render: (text) => <p>{text}</p>,
+                },
+                {
+                  title: "Email",
+                  dataIndex: "email",
+                  key: "email",
+                  sorter: (a, b) => a.email.length - b.email.length,
+                  render: (text) => <p>{text}</p>,
+                },
+                {
+                  title: "Tgl Pendaftaran",
+                  dataIndex: "created_at",
+                  key: "created_at",
+                  sorter: (a, b) => a.created_at.length - b.created_at.length,
+                  render: (text) => <p>{moment(text).format("DD/MM/YYYY")}</p>,
+                },
+                {
+                  title: "Action",
+                  key: "action",
+                  render: (_, record) => (
+                    <Space size="middle">
+                      <Button
+                        onClick={() => {
+                          handleUserStatus(
+                            record,
+                            record.user_status === "Pembaca"
+                              ? "SET_TO_AUTHOR"
+                              : "SET_TO_READER"
+                          );
+                        }}
+                        variant={
+                          record.user_status === "Pembaca"
+                            ? "success"
+                            : "primary"
+                        }
+                      >
+                        {record.user_status === "Pembaca"
+                          ? "Jadikan Penulis"
+                          : "Jadikan Pembaca"}
+                      </Button>
+                    </Space>
+                  ),
+                },
+              ]}
+              dataSource={userList}
+            />
           </div>
         </Content>
         <Footer
